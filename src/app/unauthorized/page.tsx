ad1dbd762464b722cd30ffed874aaacf3178778e
ets2121 <easytechsolution.ets@gmail.com>
2026-01-03 13:53:52 +0000

    added unauthorized page.tsx

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+export default function UnauthorizedPage() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold">🚫 Unauthorized</h1>
+        <p className="mt-2">You don’t have permission to access this page.</p>
+      </div>
+    );
+  }
+  
